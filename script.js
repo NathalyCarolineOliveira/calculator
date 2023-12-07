b4678476d782478ed832d46d01d2d30af1a21f46
NathalyCarolineOliveira <nathalycarolineadaoo@gmail.com>
2023-12-07 09:17:24 -0300

    ok

--- script.js
+++ script.js
@@ -26,5 +26,64 @@ function insert(num)
                 document.getElementById('resultado').innerHTML = "Nada..."
             }
         }
+
+    document.addEventListener("keypress", (e) => {
+        if (e.key == "1") {
+            insert("1");
+        }
+        else if (e.key == "2") {
+            insert("2");
+        }
+        else if (e.key == "3") {
+            insert("3");
+        }
+        else if (e.key == "4") {
+            insert("4");
+        }
+        else if (e.key == "5") {
+            insert("5");
+        }
+        else if (e.key == "6") {
+            insert("6");
+        }
+        else if (e.key == "7") {
+            insert("7");
+        }
+        else if (e.key == "8") {
+            insert("8");
+        }
+        else if (e.key == "9") {
+            insert("9");
+        }
+        else if (e.key == "0") {
+            insert("0");
+        }
+        else if (e.key == ".") {
+            insert(".");
+        }
+        else if (e.key == "+") {
+            insert("+");
+        }
+        else if (e.key == "-") {
+            insert("-");
+        }
+        else if (e.key == "/") {
+            insert("/");
+        }
+        else if (e.key == "*") {
+            insert("*");
+
+        }
+        else if (e.key == "<") {
+            back("<");
+        }
+        else if (e.key == "C") {
+            clean("C");
+        }
+        else if (e.key == "=") {
+            calcular("=");
+        }
+    })  
+
         
 
