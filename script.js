c6cd824c4cb072607d273673e4a0b45b18f452de
NathalyCarolineOliveira <nathalycarolineadaoo@gmail.com>
2023-12-07 07:48:18 -0300

    ok

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function insert(num)
         function clean()
         {
             document.getElementById('resultado').innerHTML = "";
+            document.querySelector('#history').innerHTML = "";
             
         }
         function back()
